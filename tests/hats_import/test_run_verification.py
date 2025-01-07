2a7a336c93db754b75bd184315abb15d7d5a6c3a
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2025-01-07 15:38:23 -0500

    Add a test to exercise the verification pipeline on a cloud catalog.

---
+++ tests/hats_import/test_run_verification.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import pandas as pd
+import pytest
+
+import hats_import.verification.run_verification as runner
+from hats_import.verification.arguments import VerificationArguments
+
+
+def test_runner(small_sky_dir_cloud, tmp_path):
+    """Runner should execute all tests and write a report to file."""
+    result_cols = ["datetime", "passed", "test", "target"]
+
+    args = VerificationArguments(input_catalog_path=small_sky_dir_cloud, output_path=tmp_path)
+    verifier = runner.run(args, write_mode="w")
+    all_passed = verifier.results_df.passed.all()
+    assert all_passed, "good catalog failed"
+    written_results = pd.read_csv(args.output_path / args.output_filename)
+    assert written_results[result_cols].equals(verifier.results_df[result_cols]), "report failed"
