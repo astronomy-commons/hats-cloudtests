74e8c5f52aab641d1440af81c1aa07a9198d8cc2
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2025-04-22 09:46:12 -0400

    Address smoke test failure.

--- tests/lsdb/catalog/test_index_search.py
+++ tests/lsdb/catalog/test_index_search.py
@@ -1,15 +1,16 @@
 import pytest
-from hats import read_hats
 
 
 @pytest.mark.requires_range
 def test_index_search(small_sky_order1_catalog_cloud, small_sky_index_dir_cloud):
-    catalog_index = read_hats(small_sky_index_dir_cloud)
-
-    index_search_catalog = small_sky_order1_catalog_cloud.index_search([900], catalog_index)
+    index_search_catalog = small_sky_order1_catalog_cloud.id_search(
+        values={"id": 900}, index_catalogs={"id": small_sky_index_dir_cloud}
+    )
     index_search_df = index_search_catalog.compute()
     assert len(index_search_df) == 0
 
-    index_search_catalog = small_sky_order1_catalog_cloud.index_search([700], catalog_index)
+    index_search_catalog = small_sky_order1_catalog_cloud.id_search(
+        values={"id": 700}, index_catalogs={"id": small_sky_index_dir_cloud}
+    )
     index_search_df = index_search_catalog.compute()
     assert len(index_search_df) == 1
