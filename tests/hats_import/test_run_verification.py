e61cd1963210ed71a4a67f6be60f129f823991d6
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2025-05-23 12:53:13 -0400

    Address smoke test failure (#79)

--- tests/hats_import/test_run_verification.py
+++ tests/hats_import/test_run_verification.py
@@ -7,8 +7,8 @@ def test_runner(small_sky_dir_cloud, tmp_path):
     """Runner should execute all tests and write a report to file."""
     result_cols = ["datetime", "passed", "test", "target"]
 
-    args = VerificationArguments(input_catalog_path=small_sky_dir_cloud, output_path=tmp_path)
-    verifier = runner.run(args, write_mode="w")
+    args = VerificationArguments(input_catalog_path=small_sky_dir_cloud, output_path=tmp_path, write_mode="w")
+    verifier = runner.run(args)
     all_passed = verifier.results_df.passed.all()
     assert all_passed, "good catalog failed"
     written_results = pd.read_csv(args.output_path / args.output_filename, comment="#")
