c36e1cd74ad0531667507b37dea0c5c381eca71a
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2025-08-29 15:04:10 -0400

    Add test to confirm filters=None

--- tests/lsdb/loaders/test_read_hats.py
+++ tests/lsdb/loaders/test_read_hats.py
@@ -1,5 +1,7 @@
 import lsdb
+import numpy as np
 from hats.pixel_math import HealpixPixel
+from hats.pixel_math.spatial_index import SPATIAL_INDEX_COLUMN
 from lsdb.catalog.margin_catalog import MarginCatalog
 
 
@@ -37,3 +39,71 @@ def test_read_hats_npix_as_dir(small_sky_npix_as_dir_cloud):
     assert isinstance(catalog, lsdb.Catalog)
     assert catalog.hc_structure.catalog_info.total_rows == len(catalog)
     assert len(catalog.compute().columns) == 5
+
+
+def test_read_hats_collection_nested_filters(small_sky_order1_dir_cloud):
+    """Tests that we appropriately handled nested pyarrow compute expressions,
+    where all clauses are AND'd together"""
+    catalog = lsdb.open_catalog(
+        small_sky_order1_dir_cloud,
+        columns=["ra", "dec"],
+        filters=[[("ra", ">", 300), ("dec", "<", -50)]],
+    )
+
+    assert isinstance(catalog, lsdb.Catalog)
+    assert all(catalog.columns == ["ra", "dec"])
+    assert catalog.hc_structure.schema.names == ["ra", "dec", SPATIAL_INDEX_COLUMN]
+    catalog_contents = catalog.compute()
+    assert np.all(catalog_contents["ra"] > 300)
+    assert np.all(catalog_contents["dec"] < -50)
+
+    assert isinstance(catalog.margin, lsdb.MarginCatalog)
+    assert all(catalog.margin.columns == ["ra", "dec"])
+    assert catalog.margin.hc_structure.schema.names == ["ra", "dec", SPATIAL_INDEX_COLUMN]
+    catalog_contents = catalog.margin.compute()
+    assert np.all(catalog_contents["ra"] > 300)
+    assert np.all(catalog_contents["dec"] < -50)
+
+
+def test_read_hats_collection_nested_filters_or(small_sky_order1_dir_cloud):
+    """Tests that we appropriately handled nested pyarrow compute expressions,
+    where clauses are OR'd together"""
+    catalog = lsdb.open_catalog(
+        small_sky_order1_dir_cloud,
+        columns=["ra", "dec"],
+        filters=[[("ra", ">", 340)], [("ra", "<", 290)]],
+    )
+
+    assert isinstance(catalog, lsdb.Catalog)
+    assert all(catalog.columns == ["ra", "dec"])
+    assert catalog.hc_structure.schema.names == ["ra", "dec", SPATIAL_INDEX_COLUMN]
+    catalog_contents = catalog.compute()
+    assert np.all(np.logical_or(catalog_contents["ra"] > 340, catalog_contents["ra"] < 290))
+
+    assert isinstance(catalog.margin, lsdb.MarginCatalog)
+    assert all(catalog.margin.columns == ["ra", "dec"])
+    assert catalog.margin.hc_structure.schema.names == ["ra", "dec", SPATIAL_INDEX_COLUMN]
+    catalog_contents = catalog.margin.compute()
+    assert np.all(np.logical_or(catalog_contents["ra"] > 340, catalog_contents["ra"] < 290))
+
+
+def test_read_hats_collection_empty_filters(small_sky_order1_dir_cloud):
+    catalog = lsdb.open_catalog(small_sky_order1_dir_cloud, filters=None)
+
+    assert isinstance(catalog, lsdb.Catalog)
+    assert all(catalog.columns == ["id", "ra", "dec", "ra_error", "dec_error"])
+
+    assert isinstance(catalog.margin, lsdb.MarginCatalog)
+    assert all(catalog.margin.columns == ["id", "ra", "dec", "ra_error", "dec_error"])
+
+    catalog = lsdb.open_catalog(
+        small_sky_order1_dir_cloud / "small_sky_order1",
+        margin_cache=small_sky_order1_dir_cloud / "small_sky_order1_margin",
+        filters=None,
+    )
+
+    assert isinstance(catalog, lsdb.Catalog)
+    assert all(catalog.columns == ["id", "ra", "dec", "ra_error", "dec_error"])
+
+    assert isinstance(catalog.margin, lsdb.MarginCatalog)
+    assert all(catalog.margin.columns == ["id", "ra", "dec", "ra_error", "dec_error"])
