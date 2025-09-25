5b657d40499f30f5fac25cd0849730349a09fb62
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2025-09-25 13:43:06 -0400

    Test and benchmark of reading CDS catalog (#91)
    * Does it work?
    
    * Add CDS benchmark
    
    * Try with healpix_29 fix
    
    * Longer timeout
    
    * Give me more, github.
    
    * are you kidding me.
    
    * Use cache_options={"parquet_bytes_io":True}
    
    * Formatting.
    
    * Update argument name
    
    * Use hats@main.

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,10 +1,11 @@
 import lsdb
+from upath import UPath
 
 
 class BenchmarksHTTP:
     """Benchmark LSDB operations via HTTP."""
 
-    timeout = 120
+    timeout = 180
     rounds = 1
     repeat = 2
     number = 1
@@ -21,6 +22,29 @@ class BenchmarksHTTP:
         self.gaia.compute()
 
     def peakmem_gaia(self):
+        self.gaia.compute()
+
+
+class BenchmarksCDSHTTP:
+    """Benchmark LSDB operations via CDS's HTTP."""
+
+    timeout = 480
+    rounds = 1
+    repeat = 2
+    number = 1
+
+    def setup(self):
+        # pylint: disable=attribute-defined-outside-init
+        self.gaia = lsdb.open_catalog(
+            UPath(
+                "https://vizcat.cds.unistra.fr/hats:n=1000000/gaia_dr3/",
+                cache_options={"parquet_precache_all_bytes": True},
+            ),
+            search_filter=lsdb.PixelSearch([(7, 114853)]),
+            columns=["DR3Name", "RA_ICRS", "DE_ICRS"],
+        )
+
+    def time_gaia(self):
         self.gaia.compute()
 
 
