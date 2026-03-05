5db04f00591c7abfa8338ed133527070bb38ae29
Sean McGuire <seanmcgu@andrew.cmu.edu>
2026-03-05 16:11:04 -0500

    Move helpers to hats testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -328,33 +328,3 @@ def xmatch_correct_cloud(local_data_dir):
 def xmatch_with_margin(local_data_dir):
     pathway = local_data_dir / "xmatch" / "xmatch_with_margin.csv"
     return file_io.load_csv_to_pandas(pathway)
-
-
-class Helpers:
-    """Helper methods for comparing catalogs"""
-
-    # pylint: disable=too-few-public-methods
-    @staticmethod
-    def assert_catalog_info_is_correct(
-        expected_catalog_info,
-        catalog_info,
-        *,
-        do_not_compare: list[str] | None = None,
-        check_extra_properties: bool = True,
-        **properties_to_update,
-    ):
-        """Check that the catalog properties are similar to the expected ones."""
-        if do_not_compare is None:
-            do_not_compare = []
-        do_not_compare.extend(["hats_creation_date", "hats_estsize"])
-        do_not_compare_dict = {prop: None for prop in do_not_compare}
-        expected_catalog_info = expected_catalog_info.copy_and_update(**do_not_compare_dict)
-        catalog_info = catalog_info.copy_and_update(**(properties_to_update | do_not_compare_dict))
-        assert expected_catalog_info.explicit_dict() == catalog_info.explicit_dict()
-        if check_extra_properties:
-            assert expected_catalog_info.extra_dict() == catalog_info.extra_dict()
-
-
-@pytest.fixture
-def helpers():
-    return Helpers()
