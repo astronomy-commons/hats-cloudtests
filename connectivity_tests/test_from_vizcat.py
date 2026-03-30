1e61218b590f47b49bc1c2cf3d0a31c7309f14aa
Sandro Campos <scampos@andrew.cmu.edu>
2026-03-30 11:37:47 -0400

    Remove pixel search from vizcat connectivity test (#141)

--- connectivity_tests/test_from_vizcat.py
+++ connectivity_tests/test_from_vizcat.py
@@ -4,7 +4,6 @@ import lsdb
 def test_from_vizcat():
     gaia = lsdb.open_catalog(
         "https://vizcat.cds.unistra.fr/hats:n=10000/gaia_dr3/",
-        search_filter=lsdb.PixelSearch([(7, 114853)]),
         columns=["DR3Name", "RA_ICRS", "DE_ICRS"],
     )
     head_frame = gaia.head(10)
